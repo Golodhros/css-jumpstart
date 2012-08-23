fe3cdcb9777319f302670147137f56e2b6456a6f
Marcos Iglesias <golodhros@gmail.com>
2012-08-22 21:00:50 -0700

    Reestructuring css jumpstart

--- design-elements/menus/RGP - Modificacion/js/rgp-dd-menu.js
+++
@@ -1,8 +0,0 @@
-function mainmenu(){
-	//Para estilizar los primeros elementos
-	$("#navmenu-h li a").not("#navmenu-h li ul li a").addClass("main-element");
-}
- 
-$(document).ready(function(){					
-	mainmenu();
-});
